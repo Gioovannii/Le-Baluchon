e4acb4ac6cdd2ba820b548178b7e5485c4c4c35a
Giovanni <44929830+Gioovannii@users.noreply.github.com>
2020-05-23 22:25:35 +0800

    Design responsive missing weather image check with sf images from apple

---
+++ Le Baluchon/Controller/WeatherViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherViewController.swift
+//  Le Baluchon
+//
+//  Created by mac on 2020/5/23.
+//  Copyright © 2020 Giovanni Gaffé. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+   
+    //MARK: - Properties
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
