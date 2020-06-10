92dbef712ce3327e511c329745e26c07545f17b6
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-06-10 20:51:56 +0800

    Simplify update and cut trailing zero

--- Le Baluchon/Controller/FixerViewController.swift
+++ Le Baluchon/Controller/FixerViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class FixerViewController: UIViewController {
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var answerLabel: UILabel!
+    
     var amount: Double = 0.00
+    var amountString: String {
+        return String(format: "%.0f", amount)
+    }
+    
     var rate: Double = 0.00
     var answer : Double = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,13 +33,14 @@ class FixerViewController: UIViewController {
                     
                     self.rate = data
                     print(self.rate)
-                    
-                    guard let amountString = self.amountTextField.text else { return }
-                    guard let DoubleString = Int(amountString) else { return }
-                    self.amount = Double(DoubleString)
+
                     guard let result = self.updateResult(self.amount, self.rate) else { return }
+                    var resultString: String {
+                        return String(format: "%.3f", result)
+                    }
+                    
+                    self.answerLabel.text = "\(self.amountString) EUR = \(resultString) USD"
                     self.amountTextField.text = ""
-                    self.answerLabel.text = "\(self.amount) EUR = \(String(describing: result)) USD"
                     
                 }
             case .failure(let error):
@@ -41,17 +48,20 @@ class FixerViewController: UIViewController {
             }
         }
     }
-    func convertResponseToDouble() -> Double {
+    
+    private func convertResponseToDouble() -> Double {
         guard let amountString = amountTextField.text else { return 0 }
         guard let DoubleString = Int(amountString) else { return 0 }
         print(Double(DoubleString))
         let amount = Double(DoubleString)
         return amount
     }
-    func updateResult(_ amount: Double, _ rate: Double) -> Double? {
-       let amountConvert =  convertResponseToDouble()
-        answer = rate * amountConvert
-        print("\(amount) EUR = \(answer) USD")
+    
+    private func updateResult(_ amount: Double, _ rate: Double) -> Double? {
+        let amountUser =  convertResponseToDouble()
+        self.amount = amountUser
+        answer = rate * amountUser
+        print("\(amountUser) EUR = \(answer) USD")
         return answer
     }
 }
